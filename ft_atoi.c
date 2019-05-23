ee81d4013eb1596081dbf78d2c96f562888bda20
Tameem CAJEE <tcajee@c4r16s1.wethinkcode.co.za>
2019-05-23 07:36:08 +0200

    completed ft_atoi and ft_strcmp

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 14:52:36 by tcajee            #+#    #+#             */
-/*   Updated: 2019/05/21 17:06:45 by tcajee           ###   ########.fr       */
+/*   Updated: 2019/05/22 08:43:58 by tcajee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,25 @@
 
 int	ft_atoi(const char *str)
 {
-	unsigned long res;
-	int sign;
-	const char *input;
+	int			res;
+	int			sign;
+	const char	*input;
 
 	res = 0;
 	input = str;
 	sign = 1;
-
-	while ((*input >= '\t' && *input <= '\r') || *input == 27
-			|| *input == ' ')
+	while ((*input >= '\t' && *input <= '\r') || *input == ' ')
+		input++;
+	if (*input == '-' || *input == '+')
 	{
+		if (*input == '-')
+			sign = -1;
 		input++;
 	}
-	while (*input == '-')
+	while (*input && ft_isdigit(*input))
 	{
-		sign *= -1;
+		res = res * 10 + (*input - '0');
 		input++;
-	}
-	while (*input)
-	{
-		if  ((*input >= '\t' && *input <= '\r') || *input == 27 \
-			|| *input == ' ')
-		{
-			return (res * sign);
-		}
-		else
-		{
-			res = res * 10 + (*input - '0');
-			input++;
-		}
 	}
 	return (res * sign);
 }
--- ft_strcmp.c
+++ ft_strcmp.c
@@ -1,1 +1,34 @@
-int strcmp(const char *s1, const char *s2);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/22 13:27:32 by tcajee            #+#    #+#             */
+/*   Updated: 2019/05/22 14:57:24 by tcajee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int i;
+	const char *str1;
+	const char *str2;
+
+	str1 = s1;
+	str2 = s2;
+	i = 0;
+	if ((str1[i] && str2[i]) && (str1[i] != str2[i]))
+		return (((unsigned char)str1[i]) - ((unsigned char)str2[i]));
+	while ((str1[i] && str2[i]) && str1[i] == str2[i])
+		i++;
+	return (((unsigned char)(str1[i])) - ((unsigned char)(str2[i])));
+}
+
+
+//return (((unsigned char)*str1) - ((unsigned char)*str2));
+//return (((unsigned char)(*str1 + 1)) - ((unsigned char)(*str2 + 1)));
+// (ft_isalpha(*str1) \ && ft_isalpha(*str2)))
