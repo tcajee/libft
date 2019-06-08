4e47b7cd977dcdacf10f1daed72badb23014c5be
Tameem CAJEE <tcajee@c5r8s1.wethinkcode.co.za>
2019-06-08 20:26:37 +0200

    fixed ft_putstr_fd

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 11:13:30 by tcajee            #+#    #+#             */
-/*   Updated: 2019/06/08 18:47:16 by tcajee           ###   ########.fr       */
+/*   Updated: 2019/06/08 20:26:06 by tcajee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	ft_putchar_fd(char c, int fd)
 {
-	write(fd, &c, sizeof(char));
+	write(fd, &c, 1);
 }
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 11:17:53 by tcajee            #+#    #+#             */
-/*   Updated: 2019/06/08 18:45:56 by tcajee           ###   ########.fr       */
+/*   Updated: 2019/06/08 20:24:38 by tcajee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,8 @@
 
 void	ft_putstr_fd(char const *s, int fd)
 {
-	int i;
-
-	i = 0;
-	if ((fd >= 0 && fd <= 2 && s))
-	{
-		while (s[i])
-			ft_putchar_fd(s[i++], fd);
-	}
+	if (!s)
+		return;
+	while (*s)
+		ft_putchar_fd(*s++, fd);
 }
