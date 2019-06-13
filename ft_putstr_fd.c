0200c5d9d444571ee8b8f392d8351b5368c80aaa
Tameem CAJEE <tcajee@c4r16s1.wethinkcode.co.za>
2019-06-13 10:50:13 +0200

    cleanup

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,15 +6,9 @@
 /*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 11:17:53 by tcajee            #+#    #+#             */
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*   Updated: 2019/06/12 16:06:17 by tcajee           ###   ########.fr       */
-=======
+/*   Updated: 2019/06/13 10:50:05 by tcajee           ###   ########.fr       */
 /*   Updated: 2019/06/08 20:24:38 by tcajee           ###   ########.fr       */
->>>>>>> 4e47b7cd977dcdacf10f1daed72badb23014c5be
-=======
 /*   Updated: 2019/06/08 20:24:38 by tcajee           ###   ########.fr       */
->>>>>>> refs/remotes/origin/master
 /*                                                                            */
 /* ************************************************************************** */
 
