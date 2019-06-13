84b4f9c81fb3554fe194e8b9de4f4e6c6b0c7380
Tameem CAJEE <tcajee@c4r16s1.wethinkcode.co.za>
2019-06-13 10:41:02 +0200

    fixed segfaults

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,11 @@
 /*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 11:17:53 by tcajee            #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2019/06/12 16:06:17 by tcajee           ###   ########.fr       */
+=======
+/*   Updated: 2019/06/08 20:24:38 by tcajee           ###   ########.fr       */
+>>>>>>> 4e47b7cd977dcdacf10f1daed72badb23014c5be
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +18,7 @@
 
 void	ft_putstr_fd(char const *s, int fd)
 {
+<<<<<<< HEAD
 	int i;
 
 	i = 0;
@@ -21,4 +26,10 @@ void	ft_putstr_fd(char const *s, int fd)
 		return ;
 	while (s[i])
 		ft_putchar_fd(s[i++], fd);
+=======
+	if (!s)
+		return;
+	while (*s)
+		ft_putchar_fd(*s++, fd);
+>>>>>>> 4e47b7cd977dcdacf10f1daed72badb23014c5be
 }
