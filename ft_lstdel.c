83bdb8002b52cd770d37eb1058e3689c6712ba02
Tameem Cajee <tcajee@student.wethinkcode.co.za>
2019-06-28 12:59:26 +0200

    fixed lstdel

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -17,7 +17,7 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 	t_list *delete;
 	t_list *next;
 
-	while (alst && *alst && del)
+	while (alst && del)
 	{
 		delete = *alst;
 		next = delete->next;
