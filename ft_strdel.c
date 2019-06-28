de602796e8d21ad72bf54ce079e4be2fc219240a
Tameem CAJEE <tcajee@c4r16s1.wethinkcode.co.za>
2019-06-28 11:10:10 +0200

    norm

--- ft_strdel.c
+++ ft_strdel.c
@@ -6,7 +6,7 @@
 /*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 14:34:47 by tcajee            #+#    #+#             */
-/*   Updated: 2019/06/28 11:01:37 by tcajee           ###   ########.fr       */
+/*   Updated: 2019/06/28 11:07:13 by tcajee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	ft_strdel(char **as)
 {
-   if (as && *as)
-   {
-	   ft_strclr(*as);
-	   ft_memdel((void *)as);
-   }
+	if (as && *as)
+	{
+		ft_strclr(*as);
+		ft_memdel((void *)as);
+	}
 }
