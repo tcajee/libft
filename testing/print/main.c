0ba68fdeb7f987270ff50090ab4b53b9162f604f
Tameem CAJEE <tcajee@c4r16s1.wethinkcode.co.za>
2019-08-19 16:42:33 +0200

    legends only

--- testing/print/main.c
+++ testing/print/main.c
@@ -6,15 +6,26 @@
 /*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/19 11:10:10 by tcajee            #+#    #+#             */
-/*   Updated: 2019/08/19 15:31:18 by tcajee           ###   ########.fr       */
+/*   Updated: 2019/08/19 16:39:48 by tcajee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 
-int main(void)
+int	main()
 {
-	/* printf("at_:	%d\n", strjoin("one", "two")); */
-	printf("ft_:	%s\n", ft_strjoin("one", "two3"));
+
+	t_wins		wins;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &wins);
+
+	/* printf("rows:		%hd\n", wins.ws_row); */
+	/* printf("columns:	%hd\n", wins.ws_col); */
+	/* printf("\033[1;31Hello world\nm;"); */
+
+	printf("\033\1331;34m        HELLO WORLD\
+			\e[0m           HELLO WORLD ");
+
+
+
 	return (0);
 }
